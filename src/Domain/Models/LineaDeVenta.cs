4bcad56b96f6cf333d6e6693fc012f0f13541584
Lucas Depetris <lucasdepetris14@gmail.com>
2024-02-20 20:54:02 -0300

    Comento logica linventa

--- src/Domain/Models/LineaDeVenta.cs
+++ src/Domain/Models/LineaDeVenta.cs
@@ -8,40 +8,38 @@ namespace Domain.Models;
 
 public partial class LineaDeVenta
 {
+    /*public LineaDeVenta(Inventario inventario, int porcentajeiva)
+    {
+        if (inventario != null && inventario.Articulo != null)
+        {
+            NetoGravado = inventario.Articulo.Costo * (1 + inventario.Articulo.MargenGanancia);
+            MontoIVA = NetoGravado * porcentajeiva;
+            Subtotal = Cantidad * (NetoGravado + MontoIVA);
+        }
+        else
+        {
+            NetoGravado = 0;
+            MontoIVA = 0;
+            Subtotal = 0;
+        }
+    }*/
+
     [Key]
     public int IdLineaDeVenta { get; set; }
 
     public int Cantidad { get; set; }
-    
+
     [Precision(18, 2)]
-    public decimal NetoGravado 
-    {
-        get
-        {
-            return Inventario.Articulo.Costo * (1 + Inventario.Articulo.MargenGanancia);
-        }
-    }
+    public decimal NetoGravado { get; private set; }
 
     [Precision(18, 2)]
     public decimal PorcentajeIVA { get; set; }
-    
+
     [Precision(18, 2)]
-    public decimal MontoIVA
-    {
-        get
-        {
-            return NetoGravado * PorcentajeIVA;
-        }
-    }
-    
+    public decimal MontoIVA { get; private set; }
+
     [Precision(18, 2)]
-    public decimal Subtotal
-    {
-        get
-        {
-            return Cantidad * (NetoGravado + MontoIVA);
-        }
-    }
+    public decimal Subtotal { get; private set; }
 
     public int IdInventario { get; set; }
     [ForeignKey("IdInventario")]
