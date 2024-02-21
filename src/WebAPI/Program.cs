039be7ef65417c9913e9865f140cb532fdf2b5bf
Lucas Depetris <lucasdepetris14@gmail.com>
2024-02-21 10:01:48 -0300

    Agrego archivos docker

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -34,6 +34,21 @@ if (app.Environment.IsDevelopment())
         options.EnableTryItOutByDefault();
     });
 }
+else if (app.Environment.IsEnvironment("Production"))
+{
+    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
+    var userPassSide = connectionUrl.Split("@")[0];
+    var hostSide = connectionUrl.Split("@")[1];
+
+    var user = userPassSide.Split(":")[0];
+    var password = userPassSide.Split(":")[1];
+    var host = hostSide.Split("/")[0];
+    var database = hostSide.Split("/")[1].Split("?")[0];
+
+    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+}
 
 app.UseHttpsRedirection();
 
